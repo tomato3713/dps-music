69024e4276ca45351379905aa6676abd4da769c7
taichi watanabe <tomato3713@outlook.com>
2021-10-26 11:17:36 +0900

    run cmd

--- denops/music/main.ts
+++ denops/music/main.ts
@@ -1,12 +1,27 @@
 import { Denops } from "https://deno.land/x/denops_std@v1.0.0/mod.ts";
 import { execute } from "https://deno.land/x/denops_std@v1.0.0/helper/mod.ts";
 import { ensureString } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
+import * as fn from "https://deno.land/x/denops_std@v2.0.1/function/mod.ts";
+import * as vars from "https://deno.land/x/denops_std@v2.0.1/variable/mod.ts";
 
 export async function main(denops: Denops): Promise<void> {
+    let ps: Deno.Process;
+
+    const debug = await vars.g.get(denops, "music_debug", true);
+    const debugLog = (...data: any[]): void => {
+        if (debug) console.log(...data);
+    };
+
     denops.dispatcher = {
         async play(title: unknown): Promise<unknown> {
             ensureString(title);
             // TODO
+            const cwd = await fn.getcwd(denops) as string;
+            const cmd = ["C:\\Windows\\explorer.exe", cwd + "\\open.mp3"] as string [];
+            debugLog( { cmd, cwd });
+            ps = Deno.run({
+                cmd: cmd,
+            });
             return await Promise.resolve(`call play api. start to play ${title}.`);
         },
 
